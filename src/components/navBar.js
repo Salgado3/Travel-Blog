74c28825945803115beab2c1f5fab50ac5ead91f
Jaime Salgado <jsalg10@gmail.com>
2021-11-30 23:45:04 -0600

    removed images. added components folder. building navbar

---
+++ src/components/navBar.js
@@ -0,0 +1,21 @@
+
+
+
+function Navbar() {
+
+    return(
+        <div className="navbar">
+            <ul>
+            <li>2020</li>
+            <li>2018</li>
+            <li>2017</li>
+            </ul>
+
+        </div>
+    )
+
+
+}
+
+
+export default Navbar
