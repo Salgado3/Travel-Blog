5b96e9c2a570c348f1cd5a5ddc9e80a5f9fb899e
Jaime Salgado <jsalg10@gmail.com>
2021-12-01 11:15:00 -0600

    sample data filled out. App working

--- src/data.js
+++ src/data.js
@@ -2,12 +2,39 @@
 
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        title: "San Miguel de Allende",
+        location: "Guanajuato, Mx",
+        googleMapsUrl: "https://www.google.com/maps/place/San+Miguel+de+Allende,+Guanajuato,+Mexico/@20.929118,-100.8515953,12.62z/data=!4m21!1m15!4m14!1m6!1m2!1s0x842b73f58b0cf1eb:0x25f4b0d165571e74!2sGuanajuato,+Mexico!2m2!1d-101.2573586!2d21.0190145!1m6!1m2!1s0x842b51af9871bc0f:0x653101031d07b0a4!2ssan+miguel+de+allende!2m2!1d-100.745235!2d20.9144491!3m4!1s0x842b51af9871bc0f:0x653101031d07b0a4!8m2!3d20.9144491!4d-100.745235",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        description: "San Miguel de Allende, a colonial-era city in Mexico’s central highlands, is known for its baroque Spanish architecture, thriving arts scene and cultural festivals. In the city’s historic, cobblestoned center lies the neo-Gothic church Parroquia de San Miguel Arcángel, whose dramatic pink towers rise above the main plaza, El Jardín. The Templo de San Francisco church nearby has an 18th-century churrigueresque facade.",
         imageUrl: "https://images.unsplash.com/photo-1518105779142-d975f22f1b0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWV4aWNvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        title: "Guanajuato",
+        location: "Guanajuato, Mx",
+        googleMapsUrl: "https://www.google.com/maps/place/Guanajuato,+Mexico/@21.025147,-101.2753898,14z/data=!3m1!4b1!4m5!3m4!1s0x842b73f58b0cf1eb:0x25f4b0d165571e74!8m2!3d21.0189766!4d-101.2573814",
+        startDate: "1 Nov, 2018",
+        endDate: "15 Nov, 2018",
+        description: "Guanajuato is a city in central Mexico. It's known for its silver mining history and colonial architecture. Its network of narrow streets, alleyways and tunnels is typified by the Callejón del Beso (Alley of the Kiss), so named because the balconies are close enough for a couple to reach across and kiss. In a former granary, the Alhóndiga de Granaditas is an art and local history museum with pre-Columbian exhibits.",
+        imageUrl: "https://images.unsplash.com/photo-1590278735512-af6c4a6044da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1992&q=80"
+    },
+    {
+        title: "Mexico City",
+        location: "Ciudad de México, Mx",
+        googleMapsUrl: "https://www.google.com/maps/place/Mexico+City,+CDMX,+Mexico/@19.3910038,-99.2837,11z/data=!3m1!4b1!4m5!3m4!1s0x85ce0026db097507:0x54061076265ee841!8m2!3d19.4326005!4d-99.1330719",
+        startDate: "9 May, 2021",
+        endDate: "29 May, 2021",
+        description: "Mexico City is the densely populated, high-altitude capital of Mexico. It's known for its Templo Mayor (a 13th-century Aztec temple), the baroque Catedral Metropolitana de México of the Spanish conquistadors and the Palacio Nacional, which houses historic murals by Diego Rivera. All of these are situated in and around the Plaza de la Constitución, the massive main square also known as the Zócalo.",
+        imageUrl: "https://images.unsplash.com/photo-1518659526054-190340b32735?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+    },
+    {
+        title: "Oaxaca City",
+        location: "Oaxaca, Mx",
+        googleMapsUrl: "https://www.google.com/maps/place/Oaxaca,+Mexico/@17.0813771,-96.7707512,13z/data=!3m1!4b1!4m5!3m4!1s0x85c72249df26d9b1:0xac88a77657dffc3b!8m2!3d17.0731842!4d-96.7265889",
+        startDate: "1 Nov, 2019",
+        endDate: "11 Nov, 2019",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://images.unsplash.com/photo-1601289127585-b9ce67af6a33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG9heGFjYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
     }
 ]
