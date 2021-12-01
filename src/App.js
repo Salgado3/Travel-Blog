98ed9d8fb2ae73711ffac2bda18e5d24f9374f99
Jaime Salgado <jsalg10@gmail.com>
2021-12-01 00:23:06 -0600

    created travel card commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Navbar from './components/navBar';
-
+import Navbar from './components/NavBar';
+import TravelCard from './components/TravelCard'
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <TravelCard/>
     </div>
   );
 }
---
+++ src/components/TravelCard.js
@@ -0,0 +1,18 @@
+
+
+
+
+function TravelCard() {
+
+return(
+    <div className="travelCard_container">
+        <h2>title goes here</h2>
+        <h3>location</h3>
+        <a href="#">googleMapsUrl</a>
+    </div>
+
+)
+
+}
+
+export default TravelCard
