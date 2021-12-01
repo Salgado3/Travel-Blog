3a101dc3e95714e748ac061eae1e3c57a74e09e8
Jaime Salgado <jsalg10@gmail.com>
2021-12-01 00:08:40 -0600

    navbar styled, image of glboe imported. flexbox added

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -5,7 +5,7 @@ function Navbar() {
 
     return(
         <div className="navbar">
-            <img src={GlobePic} />
+            <img src={GlobePic}  className="navbar_img"alt="picture of black and white globe" />
             <h1>My Travels</h1>
 
         </div>
