7b012ed71b1e291bf171c0d1bfdf664697aa31ab
Jaime Salgado <jsalg10@gmail.com>
2021-12-01 10:27:23 -0600

    added pointer. rounded edges on travel card image in css>

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -1,4 +1,4 @@
-
+import mapPointer from "../images/mapPointer.png"
 
 
 
@@ -6,14 +6,16 @@ function TravelCard(props) {
    
 return(
     <div className="travelCard_container">
-        
+
     <section className="travelCard_section"> 
         <img className="travel_img" src={`${props.imageUrl}`}/>
     </section>
 
      <section className="travelCard_section"> 
         <h2 className="travel_title">{props.title}</h2>
-        <h3 className="travel_location">{props.location}</h3>
+        <h3 className="travel_location">
+            <img className="travel_imgPointer" src={mapPointer}/>
+            {props.location}</h3>
         <a  className="travel_googleMap"href={`${props.googleMapsUrl}`}>View on Google Maps</a>
         <span className="travel_startDate">{props.startDate} - {props.endDate}</span>
         <p className="travel_endDate">{props.description}</p>
