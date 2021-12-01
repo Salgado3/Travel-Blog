3bb58764f1db8e3f4f13551e8b783e016d9155c9
Jaime Salgado <jsalg10@gmail.com>
2021-12-01 10:11:52 -0600

    added sections to travel card, touched up css to make card

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -6,12 +6,19 @@ function TravelCard(props) {
    
 return(
     <div className="travelCard_container">
-        <img src={`${props.imageUrl}`}/>
-        <h2>{props.title}</h2>
-        <h3>{props.location}</h3>
-        <a href={`${props.googleMapsUrl}`}>View on Google Maps</a>
-        <span>{props.startDate} - {props.endDate}</span>
-        <p>{props.description}</p>
+        
+    <section className="travelCard_section"> 
+        <img className="travel_img" src={`${props.imageUrl}`}/>
+    </section>
+
+     <section className="travelCard_section"> 
+        <h2 className="travel_title">{props.title}</h2>
+        <h3 className="travel_location">{props.location}</h3>
+        <a  className="travel_googleMap"href={`${props.googleMapsUrl}`}>View on Google Maps</a>
+        <span className="travel_startDate">{props.startDate} - {props.endDate}</span>
+        <p className="travel_endDate">{props.description}</p>
+    </section>
+
     </div>
 
 )
